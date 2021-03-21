5aa787367e35646aebc6112a4b67fe7d310069a0
Taimour Saeed <79660032+TaimourSaeed@users.noreply.github.com>
2021-03-21 12:20:30 +0000

    Initial Commit

---
+++ FlipAndSear/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlipAndSear
+//
+//  Created by Taimour Saeed on 21/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
